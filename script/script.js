be70ebcb99764a897974bc74e0f016900a1922f5
Facundo Conde <facundo.conde92@gmail.com>
2023-12-24 16:31:41 -0300

    fetch variation

--- script/script.js
+++ script/script.js
@@ -1,25 +1,5 @@
-async function loadAndUseTranslations() {
-    try {
-      // Fetch the JSON file containing translations
-      const response = await fetch("spanish-translation.json");
-  
-      // Check for successful response before parsing
-      if (!response.ok) {
-        throw new Error(`Error fetching translations: ${response.status}`);
-      }
-  
-      // Parse the JSON data into an object
-      const translations = await response.json();
-  
-      // Utilize the translations (example: change the content of an element)
-      const element = document.querySelector(".text");
-      element.textContent = translations["Full-Stack Web Developer"];
-  
-    } catch (error) {
-      console.error("Error loading translations:", error);
-      // Handle the error gracefully, e.g., display a message to the user
-    }
-  }
-  
-  // Call the async function to initiate the process
-  loadAndUseTranslations();
+const response = fetch("spanish-translation.json");
+const translations = await response.json();
+const translatedText = translations["Full-Stack Web Developer"];
+
+console.log(translatedText);
